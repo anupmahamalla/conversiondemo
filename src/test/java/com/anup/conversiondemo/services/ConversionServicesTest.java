1f8d7c2a56bc121331df32b102c7b9effcdf2b99
anupmahamalla <anup.mahamalla@gmail.com>
2022-04-07 11:25:23 +0530

    unit test updated

--- src/test/java/com/anup/conversiondemo/services/ConversionServicesTest.java
+++ src/test/java/com/anup/conversiondemo/services/ConversionServicesTest.java
@@ -1,49 +1,32 @@
 package com.anup.conversiondemo.services;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class ConversionServicesTest {
+    Logger logger = LoggerFactory.getLogger(ConversionServicesTest.class);
 
     @Autowired
+    @Mock
     ConversionServices conversionServices;
 
-    @BeforeEach
-    void setUp() {
-
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
     @Test
-    public void testCelToFarConverter(double celValue) {
-       double result  = conversionServices.celToFarConverter(2);
+    public void testCelToFarConverter() {
+        double result  = conversionServices.celToFarConverter(2);
+        Assert.isTrue( result== 35.6);
 
-    }
-
-    public Double farToCelConverter(double farValue) {
-        return ((farValue-32)*5)/9;
-    }
-
-    public Double inch_To_Millimeter(double inchValue) {
-        return (double)inchValue*25.4;
-    }
-
-    public Double inch_To_centimeters(double inchValue) {
-        return(double) inchValue*2.54;
-    }
-
-    public Double millimeter_To_Inch(double millValue) {
-        return (double) millValue*0.0393700787;
-    }
-
-    public Double centimeters_to_inch(double centimeterValue) {
-        return  (double)centimeterValue* 0.393700787;
     }
 }
